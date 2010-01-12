52f7d55e88d1209c99bb50fec6e9e226b30688c6
Mark Karpeles <mark@tibanne.com>
2010-01-12 03:36:57 +0000

    restarting apache

--- quick_install.sh
+++ quick_install.sh
@@ -7,5 +7,13 @@ if [ x"$APXS" = x ]; then
 	exit 1
 fi
 
+APACHECTL=`PATH="$(dirname "$APXS"):$PATH" which apache2ctl 2>/dev/null`
+
+if [ x"$APACHECTL" = x ]; then
+	echo "apache2ctl not found, make sure you have it in PATH!"
+	exit 1
+fi
+
 "$APXS" -i -a -Wc,-Wall -Wc,--std=gnu99 -n autovhost -c mod_autovhost.c
+"$APACHECTL" restart
 
