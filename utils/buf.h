db48c8770ddb6e0851fe83281b1997e09374b53c
Mark Karpeles <mark@tibanne.com>
2010-03-31 11:55:49 +0000

    I SUCK

--- utils/buf.h
+++ utils/buf.h
@@ -5,8 +5,8 @@
 #define BUF_DEFINE(_name) char *_name = NULL; size_t _name ## _pos = 0; size_t _name ## _size = 0;
 
 #define BUF_APPEND(_name, _var, _len) do { \
-	if ((_name ## _pos + _name ## _size) <= _len) { \
-		while ((_name ## _pos + _name ## _size) <= _len) \
+	if (_name ## _size <= (_name ## _pos + _len)) { \
+		while (_name ## _size <= (_name ## _pos + _len)) \
 			_name ## _size += (BUF_STEP); \
 		if (_name == NULL) \
 			_name = malloc(_name ## _size); \
